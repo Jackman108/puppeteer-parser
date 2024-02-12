ce0935a42b23176f3fb2e30d33e2b94471c9c593
Dev Eugene <71942057+Jackman108@users.noreply.github.com>
2024-02-12 18:39:20 +0300

    Using asynchronous queries

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ const browserConfig = {
 };
 
 // URL для поиска вакансий
-const vacanciesUrl = 'https://hh.ru/search/vacancy?L_save_area=true&text=React+frontend&excluded_text=&salary=&currency_code=RUR&experience=doesNotMatter&schedule=remote&order_by=salary_desc&search_period=7&items_on_page=100';
+const vacanciesUrl = 'https://hh.ru/search/vacancy?text=REACT&salary=&schedule=remote&ored_clusters=true&items_on_page=100&order_by=salary_desc&search_field=name&search_period=7&excluded_text=&hhtmFrom=vacancy_search_list&hhtmFromLabel=vacancy_search_line';
 
 // Функция для авторизации
 async function authorize(page) {
@@ -48,7 +48,7 @@ async function confirmCode(page) {
         const page = await browser.newPage();
         await page.setViewport({
             width: 1440,
-            height: 800,
+            height: 1000,
             deviceScaleFactor: 1,
         });
 
@@ -84,7 +84,92 @@ async function confirmCode(page) {
         let idx = 0;// Счётчик обработанных страниц
         let successfullySubmittedFormsCount = 0; // Счётчик успешно отправленных форм
         let unsuccessfullySubmittedFormsCount = 0; // Счётчик неуспешно отправленных форм
-        let successfullySubmittedFormsIndexes = new Set(); // Хранилище индексов успешно отправленных форм
+        let allSubmittedFormsIndexes = new Set(); // Хранилище индексов успешно отправленных форм
+
+
+        // Функция для обработки вакансии
+        const processVacancy = async (vacancyIndex, vacancyElementsSelector, page) => {
+            const vacancy = vacancyElementsSelector[vacancyIndex];
+            const button = await vacancy.$('span');
+            const text = await page.evaluate(btn => btn.textContent, button);
+
+            // Проверяем, содержит ли кнопка текст "Откликнуться" и не была ли вакансия уже обработана
+            if (text.includes('Откликнуться') && !allSubmittedFormsIndexes.has(vacancyIndex)) {
+                await new Promise(r => setTimeout(r, 1000));
+                await vacancy.click();
+
+                // Селекторы для модального окна и кнопки "Все равно откликнуться"
+                const relocationModalSelector = '.bloko-modal .bloko-modal-header [data-qa="relocation-warning-title"]';
+                const confirmButtonSelector = '[data-qa="relocation-warning-confirm"]';
+                // Ожидаем появление модального окна
+                const relocationModalHandle = await page.waitForSelector(
+                    relocationModalSelector,
+                    { visible: true, timeout: 30000 }).catch(() => null);
+
+                // Проверяем содержимое модального окна
+                if (relocationModalHandle) {
+                    const modalContent = await page.evaluate(titleModal => titleModal.textContent, relocationModalHandle);
+                    if (modalContent.includes('Вы откликаетесь на вакансию в другой стране')) {
+                        await new Promise(r => setTimeout(r, 1000));
+                        await page.click(confirmButtonSelector);
+                        console.log('Нажата кнопка "Все равно откликнуться"');
+                    }
+                }
+
+                // Ожидание другого модального окна "Отправить отклик на вакансию" или "Отклик на вакансию"
+                const responseModalSelector = '.bloko-modal-title';
+                const coverLetterToggleSelector = '[data-qa="vacancy-response-letter-toggle"]';
+                const responseSubmitSelector = '[data-qa="vacancy-response-submit-popup"] span';
+                const coverLetterInputSelector = '[data-qa="vacancy-response-popup-form-letter-input"]';
+
+                // Проверяем наличие и видимость модального окна
+                await page.waitForSelector(
+                    responseModalSelector,
+                    { visible: true, timeout: 30000 });
+                await new Promise(r => setTimeout(r, 1000));
+
+                // Нажимаем кнопку для открытия поля сопроводительного письма
+                const coverLetterButtonHandle = await page.waitForSelector(
+                    coverLetterToggleSelector,
+                    { visible: true, timeout: 30000 }).catch(() => null);
+
+                if (coverLetterButtonHandle) {
+                    await coverLetterButtonHandle.click()
+                    console.log('Нажата кнопка "Сопроводительное письмо"');
+                    await page.waitForSelector(coverLetterInputSelector, { visible: true });
+                }
+
+                // Вводим сопроводительное письмо
+                await page.type(coverLetterInputSelector, coverLetter);
+                console.log('Введено сопроводительное письмо');
+
+                // Кликаем на кнопку "Откликнуться" для отправки формы
+                await page.click(responseSubmitSelector)
+                console.log('Отправки формы на вакансию:', vacancyIndex + 1);
+
+                // Добавляем условия для проверки наличия ошибок в форме отклика
+                const isInvalidTextareaVisible = await page.evaluate(() => {
+                    const textarea = document.querySelector('textarea');
+                    return textarea && textarea.classList.contains('bloko-textarea_invalid');
+                });
+
+                const isInvalidRadioVisible = await page.evaluate(() => {
+                    const radioLabel = document.querySelector('label.bloko-radio');
+                    return radioLabel && radioLabel.classList.contains('bloko-radio_invalid');
+                });
+
+                // Проверяем наличие ошибок в форме отклика
+                if (isInvalidTextareaVisible || isInvalidRadioVisible) {
+                    console.log('Обнаружена ошибка в форме отклика');
+                    unsuccessfullySubmittedFormsCount++;
+                    await page.goBack({ waitUntil: 'domcontentloaded', timeout: 60000 });
+                    console.log('Предыдущая страница открыта');
+                } else {
+                    successfullySubmittedFormsCount++;
+                }
+                allSubmittedFormsIndexes.add(vacancyIndex);
+            }
+        };
 
         // Цикл обработки страниц
         while (pages > idx) {
@@ -98,105 +183,26 @@ async function confirmCode(page) {
                     console.log('Вакансии на странице не найдены. Завершение обработки.');
                     break; // Выход из цикла, если вакансий нет
                 }
-                if (vacancyElementsSelector.length > 0) {
 
-                    for (let vacancyIndex = 0; vacancyIndex < vacancyElementsSelector.length; vacancyIndex++) {
-                        console.log('Всех вакансий:', vacancyElementsSelector.length);
-
-                        const vacancy = vacancyElementsSelector[vacancyIndex];
-                        const button = await vacancy.$('span');
-                        const text = await page.evaluate(btn => btn.textContent, button);
-                        if (text.includes('Откликнуться') && !successfullySubmittedFormsIndexes.has(vacancyIndex)) {
-                            await new Promise(r => setTimeout(r, 1000));
-                            await vacancy.click();
-
-                            // Селекторы для модального окна и кнопки "Все равно откликнуться"
-                            const relocationModalSelector = '.bloko-modal .bloko-modal-header [data-qa="relocation-warning-title"]';
-                            const confirmButtonSelector = '[data-qa="relocation-warning-confirm"]';
-                            // Ожидаем появление модального окна
-                            const relocationModalHandle = await page.waitForSelector(
-                                relocationModalSelector,
-                                { visible: true, timeout: 30000 }).catch(() => null);
-
-                            // Проверяем содержимое модального окна
-                            if (relocationModalHandle) {
-                                const modalContent = await page.evaluate(titleModal => titleModal.textContent, relocationModalHandle);
-                                if (modalContent.includes('Вы откликаетесь на вакансию в другой стране')) {
-                                    await new Promise(r => setTimeout(r, 1000));
-                                    await page.click(confirmButtonSelector);
-                                    console.log('Нажата кнопка "Все равно откликнуться"');
-                                }
-                            }
-
-                            // Ожидание другого модального окна "Отправить отклик на вакансию" или "Отклик на вакансию"
-                            const responseModalSelector = '.bloko-modal-title';
-                            const coverLetterToggleSelector = '[data-qa="vacancy-response-letter-toggle"]';
-                            const responseSubmitSelector = '[data-qa="vacancy-response-submit-popup"] span';
-                            const coverLetterInputSelector = '[data-qa="vacancy-response-popup-form-letter-input"]';
-
-                            // Проверяем наличие и видимость модального окна
-                            await page.waitForSelector(
-                                responseModalSelector,
-                                { visible: true, timeout: 30000 });
-                            await new Promise(r => setTimeout(r, 1000));
-
-                            // Нажимаем кнопку для открытия поля сопроводительного письма
-                            const coverLetterButtonHandle = await page.waitForSelector(
-                                coverLetterToggleSelector,
-                                { visible: true, timeout: 30000 }).catch(() => null);
-
-                            if (coverLetterButtonHandle) {
-                                await coverLetterButtonHandle.click()
-                                console.log('Нажата кнопка "Сопроводительное письмо"');
-                                await page.waitForSelector(coverLetterInputSelector, { visible: true });
-                            }
-
-                            // Вводим сопроводительное письмо
-                            await page.type(coverLetterInputSelector, coverLetter);
-                            console.log('Введено сопроводительное письмо');
-
-                            // Кликаем на кнопку "Откликнуться" для отправки формы
-                            await page.click(responseSubmitSelector)
-                            console.log('Отправки формы на вакансию:', vacancyIndex + 1);
-
-                            // Добавляем условие на основе наличия блока textarea_invalid
-                            const isInvalidTextareaVisible = await page.evaluate(() => {
-                                const textarea = document.querySelector('textarea');
-                                return textarea && textarea.classList.contains('bloko-textarea_invalid');
-                            });
-
-                            // Добавляем условие на основе наличия блока radio_invalid
-                            const isInvalidRadioVisible = await page.evaluate(() => {
-                                const radioLabel = document.querySelector('label.bloko-radio');
-                                return radioLabel && radioLabel.classList.contains('bloko-radio_invalid');
-                            });
-
-                            if (isInvalidTextareaVisible || isInvalidRadioVisible) {
-                                console.log('Обнаружена ошибка в форме отклика');
-                                unsuccessfullySubmittedFormsCount++;
-                                await page.goBack({ waitUntil: 'domcontentloaded', timeout: 60000 });
-                                vacancyElementsSelector = await page.$$('[data-qa="vacancy-serp__vacancy_response"]');
-                                console.log('Предыдущая страница открыта');
-                            } else {
-                                successfullySubmittedFormsCount++;
-                            }
-                            successfullySubmittedFormsIndexes.add(vacancyIndex);
-                            console.log('Отправленных откликов:', successfullySubmittedFormsCount);
-                            console.log('Неудачных откликов:', unsuccessfullySubmittedFormsCount);
-                            console.log('Оставшихся вакансий:', vacancyElementsSelector.length - unsuccessfullySubmittedFormsCount-successfullySubmittedFormsCount);
-                        }
-                    }
+                // Обработка каждой вакансии на странице
+                for (let vacancyIndex = 0; vacancyIndex < vacancyElementsSelector.length; vacancyIndex++) {
+                    await processVacancy(vacancyIndex, vacancyElementsSelector, page);
+                    console.log('Всех вакансий:', vacancyElementsSelector.length);
+                    console.log('Обрабатываем вакансию:', vacancyIndex+1);
+                    console.log('Отправленных откликов:', successfullySubmittedFormsCount);
+                    console.log('Неудачных откликов:', unsuccessfullySubmittedFormsCount);    
                 }
+
                 // Переходим на следующую страницу, только если все вакансии обработаны
-                if (idx < pages - 1) {
-                    const nextPageButtonHandle = await page.$('[data-qa="pager-next"]');
-                    if (nextPageButtonHandle) {
-                        console.log('Переход на следующую страницу.');
-                        await nextPageButtonHandle.click();
-                        await page.waitForNavigation({ waitUntil: 'domcontentloaded', timeout: 60000 });
-                        vacancyElementsSelector = await page.$$('[data-qa="vacancy-serp__vacancy_response"]');
-                        idx++;
-                    }
+                const nextPageButtonHandle = await page.$('[data-qa="pager-next"]');
+                if (nextPageButtonHandle) {
+                    console.log('Переход на следующую страницу.');
+                    await nextPageButtonHandle.click();
+                    await page.waitForNavigation({ waitUntil: 'domcontentloaded', timeout: 60000 });
+                    idx++;
+                } else {
+                    console.log('Вакансии на последней странице обработаны.');
+                    break; // Завершаем цикл, если больше нет страниц
                 }
             } catch (err) {
                 console.error('Ошибка во время обработки страницы:', err);
